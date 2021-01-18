9827dfdf1e4e5ad50c3347eb03794e9d640157c4
Antti Juustila <antti.juustila@icloud.com>
2021-01-18 10:02:19 +0200

    Reorganizing, moving client to separate repo

--- Chat/Client/src/oy/tol/chatclient/ChatClientDataProvider.java
+++
@@ -1,9 +0,0 @@
-package oy.tol.chatclient;
-
-public interface ChatClientDataProvider {
-	String getServer();
-	String getUsername();
-	String getPassword();
-	String getNick();
-	String getEmail();
-}
